c45dab2ba26a24c0f5754d554e2fb52b42dd1226
oshri1997 <oshri19970@gmail.com>
2024-01-05 22:10:22 +0200

    change route method from get to post

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -18,7 +18,7 @@ const router = express.Router();
 router.post("/createpost", userAuth, createPost); //create post
 router.post("/", userAuth, getPosts); //get all posts
 router.get("/:postId", userAuth, getPost); //get single post by id
-router.get("/getuserpost/:userId", userAuth, getUserPost); //get user post by id
+router.post("/getuserpost/:userId", userAuth, getUserPost); //get user post by id
 router.get("/comments/:postId", userAuth, getComments); //get comments by post id
 router.post("/like/:postId", userAuth, likePost); //like post
 router.post("/likecomment/:commentId/:rid?", userAuth, likePostComment); //like comment/reply
